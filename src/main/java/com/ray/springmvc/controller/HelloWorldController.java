941ba87ff914b3f783b7e3ff800509268df3ed2a
薛磊 <xuelei@xiniunet.com>
2016-05-31 15:00:22 +0800

    spring mvc配置

---
+++ src/main/java/com/ray/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.ray.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created on 2016/5/31.
+ *
+ * @author 薛磊
+ * @since 1.0.0
+ */
+@Controller
+@RequestMapping("/springmvc")
+public class HelloWorldController {
+    @RequestMapping("/helloworld")
+    public String sayHello(){
+        System.out.println("Hello World!");
+        return "helloworld";
+    }
+}
